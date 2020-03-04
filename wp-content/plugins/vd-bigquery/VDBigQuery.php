0ee5c5e2df660d43b2c75efabb1596546750cf15
ValeriD <valeri.dobrev10@gmail.com>
2020-03-04 21:59:13 +0200

    Added some Css for the home page

--- wp-content/plugins/vd-bigquery/VDBigQuery.php
+++ wp-content/plugins/vd-bigquery/VDBigQuery.php
@@ -6,20 +6,27 @@ class VDBigQuery {
 
 	private $client;
 	private $service;
-
+	private $projectId = 'socializer-270013';
 	/**
 	 * VDBigQuery constructor.
 	 */
 	public function __construct() {
-		$this->client = $this->getClient();
-		$this->service = new Google_Service_Bigquery($this->client);
+
+//		$this->client = $this->getClient();
+//		$this->service = new Google_Service_Bigquery($this->client);
+//		var_dump($this->service->datasets->listDatasets($this->projectId));
+//		$dataset = $this->service->datasets->insert($this->projectId, new Google_Service_Bigquery_Dataset(), ['datasetId'=>'dataset1']);
+//		var_dump(json_decode(json_encode($dataset), true));
+
+
+
 	}
 
 
 	private function getClient() {
 		$client = new Google_Client();
 		$client->setApplicationName( 'Socializer' );
-		$client->setScopes( Google_Service_Bigquery::DEVSTORAGE_FULL_CONTROL );
+		$client->setScopes( Google_Service_Bigquery::BIGQUERY );
 		try {
 			$client->setAuthConfig( __DIR__ . '/credentials.json' );
 		} catch ( Google_Exception $e ) {
@@ -27,37 +34,18 @@ class VDBigQuery {
 		$client->setAccessType( 'offline' );
 		$client->setPrompt( 'select_account consent' );
 
-		$tokenPath = __DIR__ . '/token.json';
-		if ( file_exists( $tokenPath ) ) {
-			$accessToken = json_decode( file_get_contents( $tokenPath ), true );
+		if ( isset($_SESSION['google_token']) ) {
+			$accessToken = $_SESSION['google_token'];
 			$client->setAccessToken( $accessToken );
 		}
 		if ( $client->isAccessTokenExpired() ) {
 			if ( $client->getRefreshToken() ) {
 				$client->fetchAccessTokenWithRefreshToken();
 			}
-			return $client;
-
-			//else{
-//				$authUrl = $client->createAuthUrl();
-//				printf("Open the following link in your browser:\n%s\n", $authUrl);
-//				print 'Enter verification code: ';
-//				$authCode = trim(fgets(STDIN));
-//
-//				// Exchange authorization code for an access token.
-//				$accessToken = $client->fetchAccessTokenWithAuthCode($authCode);
-//				$client->setAccessToken($accessToken);
-//
-//				// Check to see if there was an error.
-//				if (array_key_exists('error', $accessToken)) {
-//					throw new Exception(join(', ', $accessToken));
-//				}
+			$_SESSION['google_token'] = $client->getAccessToken();
 		}
-		// Save the token to a file.
-//			if (!file_exists(dirname($tokenPath))) {
-//				mkdir(dirname($tokenPath), 0700, true);
-//			}
-//			file_put_contents($tokenPath, json_encode($client->getAccessToken()));
-//	    }
+
+		return $client;
+
 	}
 }
--- wp-content/plugins/vd-socializer/inc/Assets/homePage.php
+++ wp-content/plugins/vd-socializer/inc/Assets/homePage.php
@@ -3,34 +3,51 @@ include_once 'wp-load.php';
 
 get_header();
 ?>
-
+	<style>
+		.socializer-post{
+			display: inline;
+			float: left;
+			width: 40%;
+			margin-left: 5%;
+			margin-right: 5%;
+			text-align: center;
+			border-bottom: black 1px solid;
+		}
+	</style>
 	<div id="primary" class="content-area">
-		<div id="content" class="site-content" role="main">
+
+
 
 <?php
 	if ( have_posts() ) {
 
-		while ( have_posts() ) {
-			 the_post();
+		while ( have_posts() ) { ?>
+			<div class = "socializer-post">
+			 <?php the_post();
+
 			 if(get_the_author_meta('ID') === get_current_user_id()){ ?>
+
 	            <h3><?php the_title(); ?></h3>
 				<p><?php the_content();?></p>
+
 				<?php if(get_post_meta(get_the_ID(), 'post_img', true)) { ?>
-					 <img src="<?php echo get_post_meta( get_the_ID(), 'post_img', true ); ?>"
-					      style="height:500px; width:600px; position: center ">
-					 <p><?php echo get_post_meta(get_the_ID(), 'post_likes', true); ?></p>
+					 <img src="<?php echo get_post_meta( get_the_ID(), 'post_img', true ); ?>" style="display: block; margin-left: auto; margin-right: auto;width: 600px%; height: 400px">
+					 <p>Likes: <?php echo get_post_meta(get_the_ID(), 'post_likes', true); ?></p>
 					 <?php
 				 }
+
+
 			}else{
 			 	die('First you should login');
 			 }
+			 ?>
+			</div> <?php
 		}
 	}
 	else{
 		echo 'No posts';
 	}?>
 
-		</div>
 	</div>
 <?php
 
